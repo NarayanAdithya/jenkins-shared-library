c5d805c88fad2cd55d43fdfb5d2aac6301894fbd
NarayanAdithya <57533346+NarayanAdithya@users.noreply.github.com>
2022-12-10 19:04:39 +0530

    Update Groovy

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,9 +1,9 @@
 
 def call() {
     echo "Building Docker Image..."
-    withCredentials([usernamePassword(credentialsId: '', passwordVariable: 'PASS', usernameVariable: 'USER')]){
-        sh "docker build -t NarayanAdithya/nanajanashia:jma-2.0 ."
+    withCredentials([usernamePassword(credentialsId: 'dockerhub-adina', passwordVariable: 'PASS', usernameVariable: 'USER')]){
+        sh "docker build -t NarayanAdithya/nanajanashia:jmasl-2.0 ."
         sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh "docker push NarayanAdithya/nanajanashia:jma-2.0"
+        sh "docker push NarayanAdithya/nanajanashia:jmasl-2.0"
     }
 }
