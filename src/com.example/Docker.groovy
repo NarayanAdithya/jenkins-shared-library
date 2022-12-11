44f4e887652326c940c12829265179473f7d21c4
NarayanAdithya <narayanadithya1234@gmail.com>
2022-12-11 18:24:59 +0530

    Added Groovy Classes

---
+++ src/com.example/Docker.groovy
@@ -0,0 +1,19 @@
+#!/usr/bin/env groovy
+package com.example
+
+class Docker implements Serializable {
+    def script
+
+    Dcoker(script) {
+        this.script=script
+    }
+
+    def buildDockerImage(String ImageName, String Version){
+        script.echo "Building $ImageName for $BRANCH_NAME"
+        script.withCredentials([script.usernamePassword(credentialsId:'dockerhub-adina', passwordVariable: 'PASS', usernameVariable: 'USER')]){
+        script.sh "docker build -t narayanadithya/nanajanashia:$ImageName-$script.BRANCH_NAME-$Version ."
+        script.sh "echo $script.PASS | docker login -u $script.USER --password-stdin"
+        script.sh "docker push narayanadithya/nanajanashia:$ImageName-$script.BRANCH_NAME-$Version"
+    }
+    }
+}
--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,9 +1,5 @@
+import com.example
 
-def call() {
-    echo "Building Docker Image..."
-    withCredentials([usernamePassword(credentialsId: '', passwordVariable: 'PASS', usernameVariable: 'USER')]){
-        sh "docker build -t NarayanAdithya/nanajanashia:jma-2.0 ."
-        sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh "docker push NarayanAdithya/nanajanashia:jma-2.0"
-    }
+def call(String ImageName, String Version) {
+    return new Docker(this).buildDockerImage(ImageName, Version)
 }
